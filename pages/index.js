a377535cf7190b0ada07a0b00df3952caed89905
Sonal <sonaldvar@gmail.com>
2021-10-03 14:33:28 +0530

    created next app

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Hour</title>
+      </Head>
+      <h1>Login</h1>
+    </>
+  );
+};
+
+export default Home;
