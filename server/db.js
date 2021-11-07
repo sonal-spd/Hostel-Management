1458654a9cfd8ec2db69592aca35d6dce3d30500
shinigami <69804862+tada-shi@users.noreply.github.com>
2021-11-07 12:53:02 +0530

    Update db.js
    heroku database

--- server/db.js
+++ server/db.js
@@ -1,11 +1,14 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "ruv@3012",
-    host: "localhost",
+    user: "enjtgsgqhcdlyo",
+    password: "9c105ca6cbc2e7787116659e3d2dc81e6c80065417838cdd0d1f2fff7605b629",
+    host: "ec2-50-17-171-127.compute-1.amazonaws.com",
     port: 5432,
-    database: "hostel"
+    database: "d9f4b5i8reiigr"
 });
 
 module.exports = pool;
+
+
+// psql --host=ec2-50-17-171-127.compute-1.amazonaws.com  --port=5432  --username=enjtgsgqhcdlyo  --password --dbname=d9f4b5i8reiigr
