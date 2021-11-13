5d1cdd467f5ef3f5479e1209251b6db7c4a31394
Sonal <sonaldvar@gmail.com>
2021-11-13 22:52:48 +0530

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Footer from "../components/footer";
 import Header from "../components/header";
-import WebLayout from "../components/weblayout";
 import Notice from "../components/Notice";
 
 const Home = () => {
